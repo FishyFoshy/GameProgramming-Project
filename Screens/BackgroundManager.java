06b413acf3caa66b56a92d6657c2f059840f69b1
FishyFoshy <157636316+FishyFoshy@users.noreply.github.com>
2026-04-28 00:32:06 -0400

    Update BackgroundManager.java

--- Screens/BackgroundManager.java
+++ Screens/BackgroundManager.java
@@ -1,10 +1,8 @@
 package Screens;
-import ImageManip.ImageFX;
 import ImageManip.ImageManager;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
-import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
